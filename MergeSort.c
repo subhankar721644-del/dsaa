2b7ec4f9e28bc58d94bf9573cf1b9d3133eb4be0
subhankar721644-del <subhankar721644@gmail.com>
2026-09-23 01:10:52 +0530

    Add files via upload

---
+++ MergeSort.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+void merge(int a[], int low, int mid, int high)
+{
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+    int temp[100];
+
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+        {
+            temp[k] = a[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = a[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        temp[k] = a[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high)
+    {
+        temp[k] = a[j];
+        j++;
+        k++;
+    }
+
+    for (i = low, k = 0; i <= high; i++, k++)
+    {
+        a[i] = temp[k];
+    }
+}
+
+void mergeSort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+
+        merge(a, low, mid, high);
+    }
+}
+
+int main()
+{
+    int a[] = {5, 2, 8, 1, 3};
+    int n = 5;
+
+    mergeSort(a, 0, n - 1);
+
+    printf("Sorted array: ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
