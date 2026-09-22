af6755cea01812c3ae142213db6737c5d6dce6f1
subhankar721644-del <subhankar721644@gmail.com>
2026-09-23 01:13:54 +0530

    Remove comments and clean up formatting

--- 2darray,access1elementprintallelement.c
+++ 2darray,access1elementprintallelement.c
@@ -5,10 +5,7 @@ int main()
     int arr[2][3];
     int i, j;
     int row, col;
-
-    // Take 2D array input
     printf("Enter 6 elements:\n");
-
     for(i = 0; i < 2; i++)
     {
         for(j = 0; j < 3; j++)
@@ -16,14 +13,10 @@ int main()
             scanf("%d", &arr[i][j]);
         }
     }
-
-    // Access one element
     printf("Enter row and column: ");
     scanf("%d %d", &row, &col);
 
     printf("Element = %d\n", arr[row][col]);
-
-    // Print all elements
     printf("All elements:\n");
 
     for(i = 0; i < 2; i++)
@@ -34,6 +27,5 @@ int main()
         }
         printf("\n");
     }
-
     return 0;
 }
