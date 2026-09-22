e51f5fb636e9b6803320325a69026adbdb560671
subhankar721644-del <subhankar721644@gmail.com>
2026-09-23 01:19:45 +0530

    Refactor array access program and enhance output
    Removed comments and added missing print statement for all elements.

--- weptotake1darrayaccess1elementprintallelement.c
+++ weptotake1darrayaccess1elementprintallelement.c
@@ -5,20 +5,19 @@ int main()
     int arr[5];
     int i, index;
 
-    // Take 1D array input
+
     printf("Enter 5 elements:\n");
     for(i = 0; i < 5; i++)
     {
         scanf("%d", &arr[i]);
     }
 
-    // Access one element
     printf("Enter index to access: ");
     scanf("%d", &index);
 
     printf("Element = %d\n", arr[index]);
 
-    // Print all elements
+
     printf("All elements:\n");
     for(i = 0; i < 5; i++)
     {
