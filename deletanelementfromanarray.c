f5ac50233573201d90f7b9988f9ccd49c51e51bf
subhankar721644-del <subhankar721644@gmail.com>
2026-09-23 01:17:52 +0530

    Remove element from array and adjust size
    Fixed syntax issues and removed unnecessary comments.

--- deletanelementfromanarray.c
+++ deletanelementfromanarray.c
@@ -4,15 +4,9 @@ int main() {
     int arr[100] = {10, 20, 30, 40, 50};
     int n = 5;
     int position = 3;
-
-    // Shift elements to the left
     for (int i = position - 1; i < n - 1; i++) {
         arr[i] = arr[i + 1];
-    }
-
-    n--;
-
-    // Display array
+    }n--;
     printf("Array after deletion:\n");
 
     for (int i = 0; i < n; i++) {
