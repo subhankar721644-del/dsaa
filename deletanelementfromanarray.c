a8e15f822acafa5956424feaaa1d3336fd020dfb
subhankar721644-del <subhankar721644@gmail.com>
2026-09-22 22:39:20 +0530

    Add files via upload

---
+++ deletanelementfromanarray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int position = 3;
+
+    // Shift elements to the left
+    for (int i = position - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    n--;
+
+    // Display array
+    printf("Array after deletion:\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
